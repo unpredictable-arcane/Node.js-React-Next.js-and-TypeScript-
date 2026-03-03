0e81dcb0b56f6fbd83fb4784cbcd2716310724e8
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-03 20:40:23 +0530

    Practise 1 in JS

---
+++ NodeJS/JS_Refresher/play_1.js
@@ -0,0 +1,16 @@
+var name = 'Anuj';
+var age = 19;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
