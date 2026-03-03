5250f04b5964e4c85fe85fb0b60363a8a7b17d05
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-03 20:49:26 +0530

    Practise 3 in JS

---
+++ NodeJS/JS_Refresher/play_3.js
@@ -0,0 +1,27 @@
+const name = 'Anuj';
+let age = 19;
+const hasHobbies = true;
+
+age = 10;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+};
+
+// const add = (a, b) => a + b;
+// const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+// console.log(addOne(1));
+console.log(addRandom());
+
+console.log(summarizeUser(name, age, hasHobbies));
+
