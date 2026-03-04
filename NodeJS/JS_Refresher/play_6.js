40321e5e665175dc7ccc4ef18081b52b522e4f43
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-04 15:40:44 +0530

    Practise 6 in JS

---
+++ NodeJS/JS_Refresher/play_6.js
@@ -0,0 +1,17 @@
+const person = {
+  name: 'Anuj',
+  age: 19,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+};
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming');
+console.log(hobbies);
