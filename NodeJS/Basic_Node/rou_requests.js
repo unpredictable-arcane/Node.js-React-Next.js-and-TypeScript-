7fd1b61df6e46c878981e1b69109006e20b708a9
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-05 19:35:32 +0530

    Add routing request file

---
+++ NodeJS/Basic_Node/rou_requests.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter the message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"<button type = "submit">Send</button></form></body>');
+        res.write('</html>');
+    return res.end();        
+    }
+    if (url === '/message' && method === 'POST')  {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from the first trial of Node JS</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+
+server.listen(3000);
